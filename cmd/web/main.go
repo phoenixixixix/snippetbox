ec681ef31fd1515d1a109b70588a13c09a5c44db
phoenixixixix <barchukoleg12@gmail.com>
2024-02-22 02:35:41 +0200

    Initialize http.Server struct to specify defaults

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,22 +8,28 @@ import (
 )
 
 func main() {
-  addr := flag.String("addr", ":3000", "HTTP network address")
-  flag.Parse()
+	addr := flag.String("addr", ":3000", "HTTP network address")
+	flag.Parse()
 
-  infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-  errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	mux := http.NewServeMux()
 
-  mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-  fileServer := http.FileServer(http.Dir("./ui/static/"))
-  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-  infoLog.Printf("Starting server on %s", *addr)
-  err := http.ListenAndServe(*addr, mux)
-  errorLog.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
