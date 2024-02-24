a54981d9b7b492bc692cdfa77e8e38580be161f3
phoenixixixix <barchukoleg12@gmail.com>
2024-02-25 01:16:45 +0200

    Implement Insert method and use it to create record from create handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -48,5 +48,15 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	title := "Snippet second"
+	content := "Hello second Snippet\nTime to play the game"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -15,7 +15,20 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title string, content string, expiresAt int) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created_at, expires_at)
+  VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expiresAt)
+	if err != nil {
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId() // LastInsertId() method is not supported by PostgreSQL
+	if err != nil {
+		return 0, nil
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
