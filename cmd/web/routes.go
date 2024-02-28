8cceb47591fc71f461e74046204e60c474c4d1bf
phoenixixixix <barchukoleg12@gmail.com>
2024-02-28 14:43:21 +0200

    Use file embeding to get Ui files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+
+	"phoenixixixix/snippetbox/ui"
 )
 
 func (app *application) routes() http.Handler {
@@ -14,8 +16,8 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
