f12ea645603882e7c3190fb11013456a19ca8a4f
phoenixixixix <barchukoleg12@gmail.com>
2024-02-28 14:56:26 +0200

    Use generics to extend spectrum of validation for permitted values

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -79,7 +79,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 chars long")
 	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(validator.PermittedInt(form.ExpiresAt, 1, 7, 365), "expires_at", "This field must equal 1, 7, or 365")
+	form.CheckField(validator.PermittedValue(form.ExpiresAt, 1, 7, 365), "expires_at", "This field must equal 1, 7, or 365")
 
 	if !form.Valid() {
 		data := app.newTemplateData(r)
